51c9650d53a296df61470dd5fa7deea39dc91324
bjv <iconoclasthero@gmail.com>
2023-02-17 16:53:47 -0500

    cleaned up ssmv.sh

--- ssmv.sh
+++ ssmv.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #ssmv.sh: relocates screenshots from XDG_PICTURE_DIR/Screenshots to XDG_PICTURES_DIR/$ssdir
+#however, the XDG_PICTURES_DIR is not necessary and can be removed by editing the script
 
-# last edit: Fri Feb 17 03:22:10 PM EST 2023
+# last edit: Fri Feb 17 04:53:15 PM EST 2023
 
 ssdir=screenshots
 scriptname="$(realpath $0)"
@@ -15,24 +16,9 @@ fi
 
 editscript "$1"
 
-# defines the screenshot directories.  May eventually want to parse them from ~/.config/user-dirs.dirs:
-#while read -r line;
-# do
-#  if [[ "$line" =~ XDG_PICTURES ]];
-#   then
-#    xdg_pictures="${line#*=}";
-#     while [[ "$xdg_pictures" =~ \" ]];
-#      do
-#       xdg_pictures="${xdg_pictures/\"}";
-#      done
-#  fi
-#done < "$HOME/.config/user-dirs.dirs"
-
+# defines the screenshot directories:
 xdg_pictures=$(xdg-user-dir PICTURES)
 watch="$xdg_pictures/Screenshots"
 ssdest="$xdg_pictures/$ssdir"
-#echo "xdg_pictures: $xdg_pictures"
-#echo "watch: $watch"
-#echo "ssdest: $ssdest"
 
 mv "$watch"/* "$ssdest"/ && rmdir --ignore-fail-on-non-empty "$watch"
